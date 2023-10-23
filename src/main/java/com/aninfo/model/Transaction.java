a2efc6f7c3aad3d20fe73b88bc77d81830bdb183
gianella bigolin <95723211+gbigolin@users.noreply.github.com>
2023-10-22 21:38:38 -0300

    Se implementan transacciones

---
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -0,0 +1,53 @@
+package com.aninfo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="Account_Transaction")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String transactionType;
+    private Double amount;
+
+    @OneToOne
+    @JoinColumn(name = "Bank_Account", referencedColumnName = "CBU")
+    private Account cbuAssociated;
+
+    public Transaction(){
+    }
+
+    public Transaction( Account cbuAssociated, String transactionType, Double amount ) {
+        this.cbuAssociated = cbuAssociated;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    public Transaction( Long id, Account cbuAssociated, String transactionType, Double amount ) {
+        this.id = id;
+        this.cbuAssociated = cbuAssociated;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public Account getCbuAssociated() { return cbuAssociated; }
+
+    public void setCbuAssociated (Account cbuAssociated){ this.cbuAssociated = cbuAssociated;}
+
+    public String getTransactionType(){ return transactionType;}
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public Double getAmount() {return amount;}
+
+    public void setAmount(Double amount) { this.amount = amount;}
+
+}
+
